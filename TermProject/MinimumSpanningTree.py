68339ee7773cd322a1aa9581d0c3fa9e3822934f
berkeracir159@gmail.com <159.Berker>
2020-12-23 01:10:06 +0300

    add Network Layer Components

---
+++ TermProject/MinimumSpanningTree.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+from Ahc.Ahc import ComponentModel, Event, EventTypes
+
+
+class MSTComponent(ComponentModel):
+    def on_init(self, eventobj: Event):
+        print(f"{self.componentname}.{self.componentinstancenumber} - on_init")
+
+    def on_message_from_top(self, eventobj: Event):
+        #print(f"{self.componentname}.{self.componentinstancenumber} - on_message_from_top")
+        time.sleep(random.randint(0, 2))
+        event = Event(self, EventTypes.MFRT, str(eventobj.eventcontent) + "M")
+        self.send_down(event)
+
+    def on_message_from_peer(self, eventobj: Event):
+        #print(f"{self.componentname}.{self.componentinstancenumber} - on_message_from_peer")
+        pass
+
+    def on_message_from_bottom(self, eventobj: Event):
+        #print(f"{self.componentname}.{self.componentinstancenumber} - on_message_from_bottom"
+        time.sleep(random.randint(0, 2))
+        event = Event(self, EventTypes.MFRB, str(eventobj.eventcontent) + "M")
+        self.send_up(event)
+
+    def __init__(self, componentid):
+        componentname = "MSTComponent"
+        super().__init__(componentname, componentid)
